d9c29d8e5e5070b2f762e17277838c2476c1fda2
MARUFJON M. ERGASHEV <m_ergashev@uzinfocom.local>
2023-05-22 17:15:52 +0500

    new commit

--- src/app/modules/landing/home/organizers/organizers.component.ts
+++ src/app/modules/landing/home/organizers/organizers.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { GalleryService } from '../gallery/gallery.service';
 @Component({
   selector: 'app-organizers',
   templateUrl: './organizers.component.html',
@@ -7,9 +7,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OrganizersComponent implements OnInit {
 
-  constructor() { }
+  constructor(private gallery: GalleryService) { }
+  images: any[];
 
+  responsiveOptions: any[] = [
+      {
+          breakpoint: '1024px',
+          numVisible: 5
+      },
+      {
+          breakpoint: '768px',
+          numVisible: 3
+      },
+      {
+          breakpoint: '560px',
+          numVisible: 1
+      }
+  ];
   ngOnInit() {
+    this.gallery.getCategories().subscribe(res => {
+        this.images = res;
+        console.log(res);
+
+    })
   }
 
 }
